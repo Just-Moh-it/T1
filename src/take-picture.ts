6ec1c6a5bc86ffcdac8ea7d570ec21de757760a6
Mohit Yadav <mohit@mohitya.dev>
2024-05-01 15:30:32 -0400

    picture

--- src/take-picture.ts
+++ src/take-picture.ts
@@ -1,3 +1,12 @@
+import libcamera from "node-libcamera";
+import path from "path";
+
 export async function takePicture() {
-  const image = await console.log("")
+  const data = await libcamera.still({
+    output: path.resolve("./assets/capture.jpg"),
+  });
+
+  return data;
 }
+
+takePicture()
